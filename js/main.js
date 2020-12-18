2bb5c61e1d418ba929c24ca3001a80ff7d9552b9
amansdpr <amansdpr@gmail.com>
2020-12-18 16:15:42 +0600

    first upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Copy JS Start
+let myGrid = $('.cp-grid')
+let myGridItem = '.cp-grid-item'
+
+
+$('.onload-masonry').masonry({
+	itemSelector: myGridItem,
+	columnWidth: myGridItem,
+})
+$('#filter > li').on('click', function(){
+	let showme = $(this).attr('data-filter')
+	myGrid.children(myGridItem).show()
+	if (showme !== 'all') {
+		myGrid.children(':not(.' + showme + ')').hide()
+	}
+	myGrid.removeClass('onload-masonry')
+	myGrid.masonry({
+		itemSelector: myGridItem,
+		columnWidth: myGridItem,
+	})
+});
+// Copy JS End
