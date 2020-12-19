489c066a355ba14611ff86fbde2b43c73fa2609b
amansdpr <amansdpr@gmail.com>
2020-12-19 15:45:50 +0600

    first upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
 ;(function($){
     "use strict";
-	$( window ).load(function() {
+	$( document ).ready(function() {
+
+		$(window).on('load', function(){
+	
+			// Copy JS Start
+			let myGrid = $('.cp-grid')
+			let myGridItem = '.cp-grid-item'
 
 
-		// Copy JS Start
-		let myGrid = $('.cp-grid')
-		let myGridItem = '.cp-grid-item'
-
-
-		$('.onload-masonry').masonry({
-			itemSelector: myGridItem,
-			columnWidth: myGridItem,
-		})
-		$('#filter > li').on('click', function(){
-			let showme = $(this).attr('data-filter')
-			myGrid.children(myGridItem).show()
-			if (showme !== 'all') {
-				myGrid.children(':not(.' + showme + ')').hide()
-			}
-			myGrid.removeClass('onload-masonry')
-			myGrid.masonry({
+			$('.onload-masonry').masonry({
 				itemSelector: myGridItem,
 				columnWidth: myGridItem,
 			})
-		});
-		// Copy JS End
+			$('#filter > li').on('click', function(){
+				let showme = $(this).attr('data-filter')
+				myGrid.children(myGridItem).show()
+				if (showme !== 'all') {
+					myGrid.children(':not(.' + showme + ')').hide()
+				}
+				myGrid.removeClass('onload-masonry')
+				myGrid.masonry({
+					itemSelector: myGridItem,
+					columnWidth: myGridItem,
+				})
+			});
+			// Copy JS End
 
-	});
+
+		}); // windows.on.load
+	
+
+	}); // document.ready 
 
 })(jQuery)
